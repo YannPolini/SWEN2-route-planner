0472c4f87083d6be48a324838ef63da0579e6d0d
SimSta123 <sstangelberger@gmail.com>
2026-04-05 23:50:16 +0200

    MVVM in tourlogs and changed the edit and delete buttons to above the tourlogs

---
+++ SWEN_2_RoutePlanner/src/app/tourlogs.model/tourlogs.model.ts
@@ -0,0 +1,136 @@
+import { Component, signal, computed, inject, Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { TourService } from '../services/tour.service';
+
+export type Log = {
+  date: string;
+  time: string;
+  comment: string;
+  difficulty: number;
+  totalDistance: number;
+  totalTime: number;
+  rating: number;
+  tourID: string;
+  logID: number;
+  creatorName: string;
+};
+
+@Injectable({ providedIn: 'root' })
+export class TourlogsModel {
+
+  readonly logList = signal<Log[]>([
+    {
+      date: '2026-03-20',
+      time: '08:45',
+      comment: 'Angenehme Tour mit schönem Wetter und guter Sicht.',
+      difficulty: 2,
+      totalDistance: 12.4,
+      totalTime: 150,
+      rating: 4,
+      tourID: '1',
+      logID: 1,
+      creatorName: 'Demo User 2'
+    },
+    {
+      date: '2026-03-21',
+      time: '14:10',
+      comment: 'Teilweise anstrengender Anstieg, aber insgesamt sehr lohnend.',
+      difficulty: 4,
+      totalDistance: 18.7,
+      totalTime: 245,
+      rating: 5,
+      tourID: '2',
+      logID: 2,
+      creatorName: 'Demo User'
+    },
+    {
+    date: '2026-04-01',
+    time: '09:15',
+    comment: 'Sehr schöne Morgenrunde entlang des Flusses, kaum Verkehr.',
+    difficulty: 2,
+    totalDistance: 10.5,
+    totalTime: 95,
+    rating: 4,
+    tourID: '3',
+    logID: 6,
+    creatorName: 'Demo User'
+  },
+  {
+    date: '2026-04-02',
+    time: '13:40',
+    comment: 'Heißes Wetter, aber tolle Aussicht auf den Bergen.',
+    difficulty: 4,
+    totalDistance: 21.3,
+    totalTime: 280,
+    rating: 5,
+    tourID: '4',
+    logID: 7,
+    creatorName: 'Demo User'
+  },
+  {
+    date: '2026-04-03',
+    time: '17:20',
+    comment: 'Kurze Feierabendtour, entspannend und ruhig.',
+    difficulty: 1,
+    totalDistance: 5.8,
+    totalTime: 60,
+    rating: 3,
+    tourID: '5',
+    logID: 8,
+    creatorName: 'Demo User'
+  },
+  {
+    date: '2026-04-04',
+    time: '08:00',
+    comment: 'Sehr anspruchsvoll, viele steile Abschnitte.',
+    difficulty: 5,
+    totalDistance: 25.0,
+    totalTime: 340,
+    rating: 5,
+    tourID: '1',
+    logID: 9,
+    creatorName: 'Demo User'
+  },
+  {
+    date: '2026-04-05',
+    time: '11:10',
+    comment: 'Gemütliche Tour durch den Wald, ideal zum Abschalten.',
+    difficulty: 2,
+    totalDistance: 13.2,
+    totalTime: 150,
+    rating: 4,
+    tourID: '2',
+    logID: 10,
+    creatorName: 'Demo User'
+  }
+  ]);
+
+  private readonly authService = inject(AuthService);
+  private readonly tourService = inject(TourService);
+
+  readonly filteredLogs = computed(() => {
+    const selectedTourId = this.tourService.selectedTourId();
+    const currentUsername = this.authService.currentUser()?.name;
+
+    if (!selectedTourId) {
+      return [];
+    }
+
+    return this.logList().filter(log => log.tourID === selectedTourId && log.creatorName === currentUsername);
+  });
+
+  addLog(newLog: Log): void {
+    this.logList.update(logs => [newLog, ...logs]);     //newLog ist erstes im Array
+  }
+
+  updateLog(updatedLog: Log): void {
+    this.logList.update(logs =>
+      logs.map(log => log.logID === updatedLog.logID ? updatedLog : log)  //wenn logID gleich updaeteLog hier speichern, sonst das alte log
+    );
+  }
+
+  deleteLog(logID: number): void {
+    this.logList.update(logs => logs.filter(log => log.logID !== logID));
+  }
+
+}
--- SWEN_2_RoutePlanner/src/app/tourlogs/tourlogs.ts
+++ SWEN_2_RoutePlanner/src/app/tourlogs/tourlogs.ts
@@ -1,21 +1,9 @@
-import { Component, signal, computed, inject } from '@angular/core';
+import { Component, signal, computed, inject, effect } from '@angular/core';
 import { TourlogsList } from '../tourlogs-list/tourlogs-list';
-import { FormBuilder, ReactiveFormsModule, FormControl, FormGroup, Validators, FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators, FormsModule } from '@angular/forms';
 import { TourService } from '../services/tour.service';
 import { AuthService } from '../auth/auth.service';
-
-type Log = {
-  date: string;
-  time: string;
-  comment: string;
-  difficulty: number;
-  totalDistance: number;
-  totalTime: number;
-  rating: number;
-  tourID: string;
-  logID: number;
-  creatorName: string;
-};
+import { TourlogsModel, Log } from '../tourlogs.model/tourlogs.model';
 
 @Component({
   selector: 'app-home',
@@ -27,6 +15,9 @@ type Log = {
 export class Tourlogs {
 
   private readonly fb = inject(FormBuilder);
+  private readonly tourlogsModel = inject(TourlogsModel); 
+
+  readonly logList = this.tourlogsModel.logList;
 
   protected readonly logForm = this.fb.nonNullable.group({
     date: ['', [Validators.required]],
@@ -41,112 +32,11 @@ export class Tourlogs {
 
   readonly formSubmitted = signal(false);
 
-  readonly logList = signal<Log[]>([
-    {
-      date: '2026-03-20',
-      time: '08:45',
-      comment: 'Angenehme Tour mit schönem Wetter und guter Sicht.',
-      difficulty: 2,
-      totalDistance: 12.4,
-      totalTime: 150,
-      rating: 4,
-      tourID: '1',
-      logID: 1,
-      creatorName: 'Demo User 2'
-    },
-    {
-      date: '2026-03-21',
-      time: '14:10',
-      comment: 'Teilweise anstrengender Anstieg, aber insgesamt sehr lohnend.',
-      difficulty: 4,
-      totalDistance: 18.7,
-      totalTime: 245,
-      rating: 5,
-      tourID: '2',
-      logID: 2,
-      creatorName: 'Demo User'
-    },
-    {
-    date: '2026-04-01',
-    time: '09:15',
-    comment: 'Sehr schöne Morgenrunde entlang des Flusses, kaum Verkehr.',
-    difficulty: 2,
-    totalDistance: 10.5,
-    totalTime: 95,
-    rating: 4,
-    tourID: '3',
-    logID: 6,
-    creatorName: 'Demo User'
-  },
-  {
-    date: '2026-04-02',
-    time: '13:40',
-    comment: 'Heißes Wetter, aber tolle Aussicht auf den Bergen.',
-    difficulty: 4,
-    totalDistance: 21.3,
-    totalTime: 280,
-    rating: 5,
-    tourID: '4',
-    logID: 7,
-    creatorName: 'Demo User'
-  },
-  {
-    date: '2026-04-03',
-    time: '17:20',
-    comment: 'Kurze Feierabendtour, entspannend und ruhig.',
-    difficulty: 1,
-    totalDistance: 5.8,
-    totalTime: 60,
-    rating: 3,
-    tourID: '5',
-    logID: 8,
-    creatorName: 'Demo User'
-  },
-  {
-    date: '2026-04-04',
-    time: '08:00',
-    comment: 'Sehr anspruchsvoll, viele steile Abschnitte.',
-    difficulty: 5,
-    totalDistance: 25.0,
-    totalTime: 340,
-    rating: 5,
-    tourID: '1',
-    logID: 9,
-    creatorName: 'Demo User'
-  },
-  {
-    date: '2026-04-05',
-    time: '11:10',
-    comment: 'Gemütliche Tour durch den Wald, ideal zum Abschalten.',
-    difficulty: 2,
-    totalDistance: 13.2,
-    totalTime: 150,
-    rating: 4,
-    tourID: '2',
-    logID: 10,
-    creatorName: 'Demo User'
-  }
-  ]);
-
   private readonly tourService = inject(TourService);
   private readonly authService = inject(AuthService);
 
-  /*  //only visual verification during initial testing
-  readonly currentUsername = computed(() =>
-    this.authService.currentUser()?.name ?? ''
-  );
-  */
-
-  readonly filteredLogs = computed(() => {
-    const selectedTourId = this.tourService.selectedTourId();
-    const currentUsername = this.authService.currentUser()?.name;
-
-    if (!selectedTourId) {
-      return [];
-    }
-
-    return this.logList().filter(log => log.tourID === selectedTourId && log.creatorName === currentUsername);
-  });
+  //ausgelagert zu tourlogs.model.ts
+  readonly filteredLogs = this.tourlogsModel.filteredLogs;
 
   readonly canAddLog = computed(() => this.tourService.selectedTourId() !== null);
 
@@ -181,11 +71,18 @@ export class Tourlogs {
   readonly editingLogId = signal<number | null>(null);
 
   readonly selectedLog = computed(() =>
-    this.logList().find(log => log.logID === this.selectedLogId()) ?? null
+    this.tourlogsModel.logList().find(log => log.logID === this.selectedLogId()) ?? null
   );
 
+  constructor() {
+    effect(() => {      //damit wenn sich die tour wechselt das selectedLog auf null gesetzt wird
+      this.tourService.selectedTourId();
+      this.selectedLogId.set(null);
+    });
+}
+
   readonly editingLog = computed(() =>
-    this.logList().find(log => log.logID === this.editingLogId()) ?? null
+    this.tourlogsModel.logList().find(log => log.logID === this.editingLogId()) ?? null
   );
 
   selectLog(log: Log): void {
@@ -209,51 +106,12 @@ export class Tourlogs {
     this.showFormPopup.set(true);
   }
 
-  //wird das noch benutzt?
-  /*
-  closeEditPopup(): void {
-    this.showFormPopup.set(false);
-    this.editingLogId.set(null);
-  }
-    */
-  /*
-  saveEdit(): void {
-    const currentLog = this.editingLog();
-    if (!currentLog || this.logForm.invalid) return;
-
-    const formValue = this.logForm.getRawValue();
-
-    this.logList.update(logs =>
-      logs.map(log =>
-        log.logID === currentLog.logID
-          ? {
-              ...log,
-              date: formValue.date,
-              time: formValue.time,
-              comment: formValue.comment,
-              difficulty: formValue.difficulty,
-              totalDistance: formValue.totalDistance,
-              totalTime: formValue.totalTime,
-              rating: formValue.rating,
-              tourID: formValue.tourID
-            }
-          : log
-      )
-    );
-
-    this.closeEditPopup();
-  }
-    */
-
   deleteLog(): void {
     const currentLog = this.selectedLog();  //holt ausgewähltes Log damit benutzt werden kann
     
     if (!currentLog) return;
     
-    this.logList.update(logs =>
-      logs.filter(log => log.logID !== currentLog.logID)  //behält alle Logs außer dem ausgewählten.
-    );
-
+    this.tourlogsModel.deleteLog(currentLog.logID);
     this.selectedLogId.set(null);//wenn gelöscht wurde, wird das was im ausgewählten log ist zurückgesetzt
   }
 
@@ -292,25 +150,8 @@ export class Tourlogs {
       return;
     }
 
-
     if (currentLog) {
-      this.logList.update(logs =>
-        logs.map(log =>
-          log.logID === currentLog.logID
-            ? {
-              ...log,
-              date: formValue.date,
-              time: formValue.time,
-              comment: formValue.comment,
-              difficulty: formValue.difficulty,
-              totalDistance: formValue.totalDistance,
-              totalTime: formValue.totalTime,
-              rating: formValue.rating,
-              }
-            : log
-        )
-      );
-
+      this.tourlogsModel.updateLog({ ...currentLog, ...formValue });
       this.selectedLogId.set(currentLog.logID);
     } else {
       const newLog: Log = {
@@ -326,7 +167,7 @@ export class Tourlogs {
         creatorName: currentUsername,
       };
 
-      this.logList.update(logs => [newLog, ...logs]);
+      this.tourlogsModel.addLog(newLog);
       this.selectedLogId.set(newLog.logID);
     }
 
